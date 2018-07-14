b3f26b7d92fc2fa2486053114fb0608d62111df2
Julius Jacobsohn <julius.jacobsohn@hotmail.com>
2018-07-14 14:50:01 +0200

    [Misc] Removed unnecessary files

--- example08-presentation/target/dart/web/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:angular/angular.dart';import 'main.template.dart' as ngStaticInit;
-import 'package:example08/app_component.dart';
-
-void main() {ngStaticInit.initReflector();
-  bootstrapStatic(AppComponent);
-}
